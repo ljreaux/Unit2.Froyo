b7615a2f8194e08535b27e956d1d3c549156b8d8
Larry John Reaux <larryreaux@Larrys-Air.attlocal.net>
2023-11-05 17:04:14 -0600

    fixed code to take any flavor input

--- script.js
+++ script.js
@@ -16,33 +16,24 @@ const stringArray = userInputString.split(",");
 
 // 3. Construct an Object that tracks the number of each flavor.
 
-const flavorTracker = {
-  vanilla: 0,
-  strawberry: 0,
-  chocolate: 0,
-  coffee: 0,
-}
+const flavorTracker = {}
 
 // 4. Update the oject as new input comes in.
 
-const flavors = Object.keys(flavorTracker)
+
 
 // 5. Organize logic into a function.
 
 function trackFlavors() {
 
   for (i = 0; i < stringArray.length; i++) {
-    if (stringArray[i] == flavors[0]) {
-      flavorTracker["vanilla"]++
-    } else if (stringArray[i] == flavors[1]) {
-      flavorTracker["strawberry"]++
-    } else if (stringArray[i] == flavors[2]) {
-      flavorTracker['chocolate']++
-    } else if (stringArray[i] == flavors[3]) {
-      flavorTracker['coffee']++
+    const index = stringArray[i]
+    if (flavorTracker[index] === undefined) {
+      flavorTracker[index] = 1
     } else {
-      return 'Enter a valid Froyo Order!'
+      flavorTracker[index]++
     }
   } return flavorTracker
 }
-console.log("Current Orders: "+ JSON.stringify(trackFlavors()))
+console.log("Current Orders: ")
+console.table(trackFlavors())
